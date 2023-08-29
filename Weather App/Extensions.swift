c69d8181c88e19499c67306074f82467ebaa6a4d
John Suman <john@onigo.club>
2023-08-29 12:43:28 +0900

    get openweahter api and set model data

---
+++ Weather App/Extensions.swift
@@ -0,0 +1,14 @@
+//
+//  Extensions.swift
+//  Weather App
+//
+//  Created by John Suman on 2023/08/29.
+//
+
+import Foundation
+
+extension Double {
+    func roundDouble() -> String {
+        return String(format: "%.0f", self)
+    }
+}
---
+++ Weather App/Views/WeatherView.swift
@@ -0,0 +1,20 @@
+//
+//  WeatherView.swift
+//  Weather App
+//
+//  Created by John Suman on 2023/08/29.
+//
+
+import SwiftUI
+
+struct WeatherView: View {
+    var weather: ResponseBody
+    
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    WeatherView(weather: previewWeather)
+}
